8fcb96cf4dd6882bf92b4734d068a58871d5709e
stelioszlat <steliosdj6@gmail.com>
2021-12-14 15:49:05 +0200

    added exhibit validation methods - not implemented

---
+++ src/validation/exhibitValidation.js
@@ -0,0 +1,15 @@
+const { body, validationResult} = require('express-validator');
+const Exhibit = require('../models/Exhibit');
+const error = require('../shared/error');
+
+exports.validateName = value => {}
+
+exports.validateDescription = value => {}
+
+exports.validateImageLocation = value => {}
+
+exports.validateSoundLocation = value => {}
+
+exports.validateValue = value => {}
+
+exports.validateImageLocation = value => {}

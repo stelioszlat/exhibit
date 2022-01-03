1319421c3c116ffabde39b76db1295b9cbe69749
Stelios Zlat <steliosdj6@gmail.com>
2022-01-03 13:08:42 -0500

    added file storage functions

---
+++ src/util/file-storage.js
@@ -0,0 +1,31 @@
+
+const multer = require('multer');
+
+exports.imageFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg')
+    {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+exports.imageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + "-" + file.originalname);
+    } 
+});
+
+exports.museumImageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})

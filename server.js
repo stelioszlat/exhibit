910919877f9d606f6bba2ef4c5385a9d5b5cedee
Stelios Zlat <steliosdj6@gmail.com>
2022-01-03 13:05:49 -0500

    added multer for file uploading

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const multer = require('multer');
 
 const { connect } = require('./src/db/connect');
 const config = require('./config.json');
 const logger = require('./src/util/logger');
 const apiLogger = require('./src/util/api-logger');
 const error = require('./src/shared/error');
+const fileStorage = require('./src/util/file-storage');
 
 const host = config.host;
 const port = config.port;
@@ -20,13 +22,17 @@ const adminRoutes = require('./src/routes/admin-routes');
 const app = express();
 app.use(apiLogger);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'static')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(multer({storage: fileStorage.imageStorage, fileFilter: fileStorage.imageFilter}).single('image'));
+app.use(multer({storage: fileStorage.museumImageStorage, fileFilter: fileStorage.imageFilter})).single('image');
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(cors());
 app.use('/exhibits', exhibitRoutes);
 app.use('/sections', sectionRoutes);
 app.use('/museums', museumRoutes);
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);             // requires authentication
 app.use(error.errorHandler);
 
 connect(config.db);
